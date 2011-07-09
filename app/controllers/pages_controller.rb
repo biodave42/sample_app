f388f6528dce6cff38c44c41f84e93dc1cf2e437
Dave Hodgson <dave@biodave.com>
2011-07-09 16:53:34 -0700

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def content
+    @title = "content"
+  end
+
+  def about
+    @title = 'about'
   end
 
 end
